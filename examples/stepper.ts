6d2867d9c8d0b0eda1bc8e847052a4a660cd35b9
Jesse Alama <jesse@igalia.com>
2025-05-22 16:11:40 +0200

    Add exaple of stepping up a value

---
+++ examples/stepper.ts
@@ -0,0 +1,20 @@
+import { Decimal } from "../src/Decimal.mjs";
+
+function stepUp(a: Decimal.Amount, n?: number): Decimal.Amount {
+    if (undefined === n) {
+        n = 1;
+    }
+    if (!Number.isInteger(n)) {
+        throw new RangeError("n must be an integer");
+    }
+    let stepped = a.toDecimal();
+    for (let i = 0; i < n; i++) {
+        stepped = stepped.add(a.quantum);
+    }
+    return Decimal.Amount.from(stepped.toFixed({ digits: a.fractionalDigits }));
+}
+
+let a = Decimal.Amount.from("42.9").withFractionalDigits(2);
+let stepped = stepUp(a, 9);
+console.log(`a: ${a.toString()} stepped up 9 times is ${stepped.toString()}`);
+console.log(`and one more time yields ${stepUp(stepped).toString()}`);
