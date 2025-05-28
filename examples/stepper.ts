949d9ed9b9af528494c8c6fe8030e971943957fe
Jesse Alama <jessealama@users.noreply.github.com>
2025-05-28 18:43:32 +0200

    Simplify `.with` methods (#36)
    Also, say "fraction digit" in favor of "fractional digits".

--- examples/stepper.ts
+++ examples/stepper.ts
@@ -14,7 +14,7 @@ function stepUp(a: Decimal.Amount, n?: number): Decimal.Amount {
     return Decimal.Amount.from(stepped.toFixed({ digits: a.fractionalDigits }));
 }
 
-let a = Decimal.Amount.from("42.9").with({ kind: "fractionDigit", digits: 2 });
+let a = Decimal.Amount.from("42.9").with({ fractionDigit: 2 });
 let stepped = stepUp(a, 9);
 console.log(`a: ${a.toString()} stepped up 9 times is ${stepped.toString()}`);
 console.log(`and one more time yields ${stepUp(stepped).toString()}`);
